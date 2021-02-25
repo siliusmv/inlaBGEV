738539f3defc56ea97d47175cf3f1497441df43d
Silius M. Vandeskog <siliusv@gmail.com>
2021-02-25 08:26:37 +0100

    Speed up the simulation study by using parallelisation

--- exec/simulation-study.R
+++ exec/simulation-study.R
@@ -2,6 +2,7 @@ library(INLA)
 library(ggplot2)
 library(dplyr)
 library(inlaBGEV)
+library(parallel)
 
 n_trials = 20
 n_loc = 250
@@ -9,10 +10,13 @@ n = 1500
 α = .5
 β = .8
 n_trials = 30
-n_standardised_samples = 10
+n_standardised_samples = 12
+num_cores = 12
+
+#inla.setOption(pardiso.license = "~/.R/licences/pardiso.lic")
 
 tail_estimates = list()
-set.seed(123)
+set.seed(1, kind = "L'Ecuyer-CMRG")
 for (i in 1:n_trials) {
   # Use a location parameter of 0, draw random coefficients for s and a random tail parameter
   q = rep(0, n_loc)
@@ -50,57 +54,59 @@ for (i in 1:n_trials) {
     inla_bgev(
       data = data,
       covariate_names = list(NULL, s_cov_names, NULL),
+      α = α,
+      β = β,
       response_name = "obs"),
     error = function(e) NULL)
   #summary(res1)
 
-  samples = list()
-  for (j in 1:n_standardised_samples) {
-    # Add some noise to s and standardise the observations
-    noisy_s = s[location_indices] * rnorm(n, mean = 1, sd = .1)
-
-    res2 = tryCatch(
-      inla_bgev(
-        data = data,
-        s_est = noisy_s,
-        covariate_names = list(NULL, NULL, NULL),
-        response_name = "obs"),
-      error = function(e) NULL)
-    #summary(res2)
-
-    # Sample ξ from the posterior of res2
-    if (!is.null(res2)) {
-      samples[[length(samples) + 1]] = INLA::inla.posterior.sample(100, res2) %>%
+  # Run R-inla to estimate the BGEV-parameters once for each of the SD samples
+  samples = parallel::mclapply(
+    X = seq_len(n_standardised_samples),
+    mc.cores = num_cores,
+    mc.preschedule = FALSE,
+    FUN = function(i) {
+      noisy_s = s[location_indices] * rnorm(n, mean = 1, sd = .1)
+      res = tryCatch({
+        inla_bgev(
+          data = data,
+          s_est = noisy_s,
+          covariate_names = list(NULL, NULL, NULL),
+          response_name = "obs",
+          α = α,
+          β = β)},
+        error = function(e) NULL)
+      message("Done with iter nr. ", i)
+      if (is.null(res) || !res$convergence) return(NULL)
+      inla.posterior.sample(100, res, seed = 1) %>%
         sapply(function(x) x$hyper[2])
-    }
-  }
+    })
 
-  tail_estimates[[i]] = list(truth = ξ)
 
+  tail_estimates[[i]] = list(truth = ξ, n_s = n_s)
   # res1 might some times crash form numerical errors. Sometimes it also says that
   # ξ is nondegenerate in its summary, but not when we sample from it. Therefore,
   # we might have to sample ξ from the posterior of res1. Since the crashes are not
   # 'consistent', we have to preserve the random seed here to ensure as much
   # reproducibility as possible
   seed = .Random.seed
-  if (!is.null(res1) && res$convergence) {
-    tail_estimates[[i]]$with_spread = summary(res1)$hyper[2, -6]
+  if (!is.null(res1)) {
+    tail_estimates[[i]]$joint = summary(res1)$hyper[2, -6]
     if (any(is.na(summary(res1)$hyper[2, ]))) {
       tmp = INLA::inla.posterior.sample(100, res1) %>%
         sapply(function(x) x$hyper[2]) %>%
         data_stats(q = c(.025, .5, .975))
       names(tmp) = c("mean", "sd", "0.025quant", "0.5quant", "0.975quant")
-      tail_estimates[[i]]$with_spread = tmp
+      tail_estimates[[i]]$joint = tmp
     }
   }
   .Random.seed = seed
 
-  tail_estimates[[i]]$n_s = n_s
-
+  if (any(sapply(samples, is.null))) samples = samples[-which(sapply(samples, is.null))]
   tmp = unlist(samples) %>%
     data_stats(q = c(.025, .5, .975))
   names(tmp) = c("mean", "sd", "0.025quant", "0.5quant", "0.975quant")
-  tail_estimates[[i]]$standardised = tmp
+  tail_estimates[[i]]$twostep = tmp
   message("Done with iteration nr. ", i)
 }
 
@@ -108,18 +114,18 @@ mydf = lapply(
   seq_along(tail_estimates),
   function(i) {
     x = tail_estimates[[i]]
-    if (is.null(x$with_spread)) {
-      x$with_spread = data.frame(NA, NA, NA, NA, NA)
-      names(x$with_spread) = c("mean", "sd", "0.025quant", "0.5quant", "0.975quant")
+    if (is.null(x$joint)) {
+      x$joint = data.frame(NA, NA, NA, NA, NA)
+      names(x$joint) = c("mean", "sd", "0.025quant", "0.5quant", "0.975quant")
     }
-    if (is.null(x$standardised)) {
-      x$standardised = data.frame(NA, NA, NA, NA, NA)
-      names(x$standardised) = c("mean", "sd", "0.025quant", "0.5quant", "0.975quant")
+    if (is.null(x$twostep)) {
+      x$twostep = data.frame(NA, NA, NA, NA, NA)
+      names(x$twostep) = c("mean", "sd", "0.025quant", "0.5quant", "0.975quant")
     }
-    res = as.data.frame(rbind(x$with_spread, x$standardised))
+    res = as.data.frame(rbind(x$joint, x$twostep))
     res$tag = c("Modelling all parameters jointly",
-                "Modelling the spread separately")[
-                  c(!is.null(x$with_spread), !is.null(x$standardised))]
+                "Modelling with a twostep procedure")[
+                  c(!is.null(x$joint), !is.null(x$twostep))]
     res$truth = x$truth
     res$x = i
     res$n_s = x$n_s
