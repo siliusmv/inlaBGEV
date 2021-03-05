6502efe082e1d161784c667aa39a8ec514c06c4d
Silius M. Vandeskog <siliusv@gmail.com>
2021-03-05 14:41:21 +0100

    Plot the station locations

---
+++ exec/visualise-data.R
@@ -0,0 +1,8 @@
+library(inlaBGEV)
+library(ggplot2)
+library(sf)
+
+plot = plot_on_map(observations, use_tex = TRUE)
+
+tikz_plot(file.path(here::here(), "inst", "extdata", "station-locations.pdf"),
+          plot, width = 10, height = 7, view = TRUE)
