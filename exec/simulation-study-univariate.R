6fe744f168c4417e189f9d507f25b7b70afef698
Silius M. Vandeskog <siliusv@gmail.com>
2021-03-29 08:15:38 +0200

    Update univariate simulation study

--- exec/simulation-study-univariate.R
+++ exec/simulation-study-univariate.R
@@ -10,21 +10,20 @@ n_trials = 2000
 num_cores = 20
 return_periods = c(5, 10, 25, 50)
 
-inclusion_percentages = list()
-for (i in seq_along(n_vec)) {
-  n = n_vec[i]
-  set.seed(1, kind = "L'Ecuyer-CMRG") # Set a seed that works for paralellisation
-  inclusion = parallel::mclapply(
-    X = seq_len(n_trials),
-    mc.preschedule = FALSE,
-    mc.cores = num_cores,
-    FUN = function(j) {
-      q = rnorm(1, 0, 5)
-      s = runif(1, 0, 10)
-      ξ = runif(1, 0, .4)
-      locscale_pars = locspread_to_locscale(q, s, ξ, α, β)
-      μ = locscale_pars$μ; σ = locscale_pars$σ
-      return_levels = return_level_bgev(return_periods, μ, σ, ξ)
+set.seed(1, kind = "L'Ecuyer-CMRG") # Set a seed that works for paralellisation
+stats = parallel::mclapply(
+  X = seq_len(n_trials),
+  mc.cores = num_cores,
+  mc.preschedule = FALSE,
+  FUN = function(i) {
+    q = rnorm(1, 0, 5)
+    s = runif(1, 0, 10)
+    ξ = runif(1, 0, .4)
+    locscale_pars = locspread_to_locscale(q, s, ξ, α, β)
+    μ = locscale_pars$μ; σ = locscale_pars$σ
+    return_levels = return_level_bgev(return_periods, μ, σ, ξ)
+    stats = list()
+    for (n in n_vec) {
       y = rgev(n, μ, σ, ξ)
       res = tryCatch(inla_bgev(y, α = α, β = β), error = function(e) NULL)
       if (is.null(res)) return(NULL)
@@ -36,52 +35,94 @@ for (i in seq_along(n_vec)) {
       μ_s = locscale_s$μ; σ_s = locscale_s$σ
       est_return_levels = mapply(return_level_bgev, μ = μ_s, σ = σ_s, ξ = ξ_s,
                                  MoreArgs = list(period = return_periods))
-
       pars = c("q", "s", "ξ", paste(return_periods, "return period"))
-      is_inside = NULL; lower = NULL; upper = NULL; mean_vals = NULL
+      is_inside = NULL; lower = NULL; upper = NULL; mean_vals = NULL; mse = NULL
       values = c(q, s, ξ, return_levels)
       for (var in c("q", "s", "ξ")) {
         interval = quantile(get(paste0(var, "_s")), c(.025, .975))
         is_inside = c(is_inside, get(var) >= interval[1] && get(var) <= interval[2])
         lower = c(lower, interval[1]); upper = c(upper, interval[2])
         mean_vals = c(mean_vals, mean(get(paste0(var, "_s"))))
+        mse = c(mse, mean((get(paste0(var, "_s")) - get(var))^2))
       }
-      for (i in seq_along(return_periods)) {
-        interval = quantile(est_return_levels[i, ], c(.025, .975))
-        is_inside = c(is_inside, return_levels[i] >= interval[1] && return_levels[i] <= interval[2])
+      for (j in seq_along(return_periods)) {
+        interval = quantile(est_return_levels[j, ], c(.025, .975))
+        is_inside = c(is_inside, return_levels[j] >= interval[1] && return_levels[j] <= interval[2])
         lower = c(lower, interval[1]); upper = c(upper, interval[2])
-        mean_vals = c(mean_vals, mean(est_return_levels[i, ]))
+        mean_vals = c(mean_vals, mean(est_return_levels[j, ]))
+        mse = c(mse, mean((est_return_levels[j, ] - return_levels[j])^2))
       }
-      data.frame(par = pars,
-                 val = values,
-                 inside = is_inside,
-                 n = n, j = j,
-                 lower = lower,
-                 mean = mean_vals,
-                 upper = upper,
-                 CI_width = upper - lower,
-                 err = abs(mean_vals - values))
-    })
-  inclusion = inclusion[!sapply(inclusion, is.null)]
-  inclusion = do.call(rbind, inclusion)
-  inclusion_percentages[[i]] = lapply(
-    X = seq_along(unique(inclusion$par)),
-    FUN = function(i) {
-      par = unique(inclusion$par)[i]
-      df = dplyr::filter(inclusion, par == !!par)
-      data.frame(par = par, n = inclusion$n[1],
-                 inclusion_percentage = mean(df$inside),
-                 CI_width = mean(df$CI_width),
-                 err = mean(df$err))
-    })
-  inclusion_percentages[[i]] = do.call(rbind, inclusion_percentages[[i]])
-  message("Done with n = ", n, ". Number of successful runs: ", length(unique(inclusion$j)))
-  print(inclusion_percentages[[i]])
-  saveRDS(
-    inclusion_percentages,
-    file.path(here::here(), "inst", "extdata", "simulation-study-univariate.rds"))
-}
+      stwcrps = mean(stwcrps_bgev(y, μ_s, σ_s, ξ_s, .9))
+      stwcrps_true = mean(stwcrps_bgev(y, μ, σ, ξ, .9))
+      stats[[n]] = data.frame(
+        par = pars,
+        val = values,
+        inside = is_inside,
+        n = n, i = i,
+        lower = lower,
+        mean = mean_vals,
+        mse = mse,
+        stwcrps = mean(stwcrps),
+        stwcrps_true = mean(stwcrps_true),
+        upper = upper,
+        CI_width = upper - lower)
+    }
+    stats = do.call(rbind, stats)
+    stats
+  })
 
 saveRDS(
-  inclusion_percentages,
+  stats,
   file.path(here::here(), "inst", "extdata", "simulation-study-univariate.rds"))
+
+#  inclusion = inclusion[!sapply(inclusion, is.null)]
+#  inclusion = do.call(rbind, inclusion)
+#  inclusion_percentages[[i]] = lapply(
+#    X = seq_along(unique(inclusion$par)),
+#    FUN = function(i) {
+#      par = unique(inclusion$par)[i]
+#      df = dplyr::filter(inclusion, par == !!par)
+#      data.frame(par = par, n = inclusion$n[1],
+#                 inclusion_percentage = mean(df$inside),
+#                 CI_width = mean(df$CI_width),
+#                 stwcrps = mean(df$stwcrps),
+#                 stwcrps_true = mean(df$stwcrps_true),
+#                 err = mean(df$err^2))
+#
+#      
+#
+#    }
+#  }
+#)
+#
+#
+#    
+#
+#stop("You should do this the other way, so we use the same q, s, ξ for all values of n. Then we can actually see that the error goes down as n increases!!!")
+#inclusion_percentages = list()
+#for (i in seq_along(n_vec)) {
+#  n = n_vec[i]
+#  set.seed(1, kind = "L'Ecuyer-CMRG") # Set a seed that works for paralellisation
+#  inclusion = parallel::mclapply(
+#    X = seq_len(n_trials),
+#    mc.preschedule = FALSE,
+#    mc.cores = num_cores,
+#    FUN = function(j) {
+#    })
+#  inclusion_percentages[[i]] = do.call(rbind, inclusion_percentages[[i]])
+#  message("Done with n = ", n, ". Number of successful runs: ", length(unique(inclusion$j)))
+#  print(inclusion_percentages[[i]])
+#  saveRDS(
+#    inclusion_percentages,
+#    file.path(here::here(), "inst", "extdata", "simulation-study-univariate.rds"))
+#}
+#
+#saveRDS(
+#  inclusion_percentages,
+#  file.path(here::here(), "inst", "extdata", "simulation-study-univariate.rds"))
+#
+#inclusion_percentages %>%
+#  do.call(rbind, .) %>%
+#  dplyr::select(par, n, inclusion_percentage) %>%
+#  tidyr::pivot_wider(names_from = "n", values_from = "inclusion_percentage") %>%
+#  xtable::xtable()
