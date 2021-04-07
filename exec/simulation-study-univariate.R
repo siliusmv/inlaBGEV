b8df041ab99a01366b8edfebcfe79a6be7d04154
Silius M. Vandeskog <siliusv@gmail.com>
2021-04-07 21:01:31 +0200

    Update univariate simulation study

--- exec/simulation-study-univariate.R
+++ exec/simulation-study-univariate.R
@@ -4,6 +4,45 @@ library(dplyr)
 library(inlaBGEV)
 library(parallel)
 
+
+#mle_bgev = function(x, p_a = .1, p_b = .2) {
+#  f = function(par, x) {
+#    res = -sum(dbgev(x, par[1], par[2], par[3], p_a, p_b, log = TRUE))
+#    if (is.infinite(res)) res = 99999999 * sign(res)
+#    res
+#  }
+#  optim(c(0, 1, .1), fn = f, lower = c(-100, .00001, .00001), upper = c(100, 100, .5),
+#        method = "L-BFGS-B", x = x)
+#}
+
+# set.seed(7)
+# q = 2.3
+# s = 1
+# ξ = .23
+# α = .5
+# β = .8
+# locscale_pars = locspread_to_locscale(q, s, ξ, α, β)
+# μ = locscale_pars$μ; σ = locscale_pars$σ
+# y = rgev(10000, μ, σ, ξ)
+# standardising_const = diff(quantile(y, c(.05, .95)))
+# y = y / standardising_const
+# mdata = inla.mdata(y, matrix(0, length(y), 0), matrix(0, length(y), 0))
+# 
+# r = inla(
+#   formula = mdata ~ -1 + intercept,
+#   data = data.frame(intercept = 1),
+#   control.family = list(control.bgev = list(q.location = α, q.spread = β)),
+#   family = "bgev")
+# summary(r)$hyper
+# truth = data.frame(q = q / standardising_const, s = s / standardising_const, ξ = ξ)
+# rownames(truth) = NULL
+# truth
+
+#r2 = mle_bgev(y)
+#p2 = locscale_to_locspread(r2$par[1], r2$par[2], r2$par[3], α, β)
+#p2
+
+
 expected_twcrps_bgev_gev = function(μ, σ, ξ, p,
                                     μ_true = μ, σ_true = σ, ξ_true = ξ,
                                     p_a = .1, p_b = .2) {
@@ -23,9 +62,9 @@ expected_twcrps_bgev_gev = function(μ, σ, ξ, p,
 
 α = .5; β = .8 # Probabilities used in the location and spread parameters
 n_vec = c(10, 50, 100, 500, 1000, 5000, 10000)
-n_trials = 1000
+n_trials = 500
 num_cores = 25
-return_periods = c(5, 10, 25, 50)
+return_periods = c(5, 10, 25, 50, 100, 200, 500)
 
 set.seed(1, kind = "L'Ecuyer-CMRG") # Set a seed that works for paralellisation
 stats = parallel::mclapply(
@@ -39,57 +78,58 @@ stats = parallel::mclapply(
     locscale_pars = locspread_to_locscale(q, s, ξ, α, β)
     μ = locscale_pars$μ; σ = locscale_pars$σ
     return_levels = return_level_bgev(return_periods, μ, σ, ξ)
-    stats = list()
-    for (n in n_vec) {
-      y = rgev(n, μ, σ, ξ)
-      res = tryCatch(inla_bgev(y, α = α, β = β), error = function(e) NULL)
-      if (is.null(res)) return(NULL)
-      samples = INLA::inla.posterior.sample(500, res, seed = 1)
-      q_s = vapply(samples, function(x) utils::tail(x$latent, 1), 1) * res$standardising_const
-      s_s = vapply(samples, function(x) x$hyperpar[1], 1) * res$standardising_const
-      ξ_s = vapply(samples, function(x) x$hyperpar[2], 1)
-      locscale_s = locspread_to_locscale(q_s, s_s, ξ_s, α, β)
-      μ_s = locscale_s$μ; σ_s = locscale_s$σ
-      est_return_levels = mapply(return_level_bgev, μ = μ_s, σ = σ_s, ξ = ξ_s,
-                                 MoreArgs = list(period = return_periods))
-      pars = c("q", "s", "ξ", paste(return_periods, "return period"))
-      is_inside = NULL; lower = NULL; upper = NULL; mean_vals = NULL; mse = NULL
-      values = c(q, s, ξ, return_levels)
-      for (var in c("q", "s", "ξ")) {
-        interval = quantile(get(paste0(var, "_s")), c(.025, .975))
-        is_inside = c(is_inside, get(var) >= interval[1] && get(var) <= interval[2])
-        lower = c(lower, interval[1]); upper = c(upper, interval[2])
-        mean_vals = c(mean_vals, base::mean(get(paste0(var, "_s"))))
-        mse = c(mse, base::mean((get(paste0(var, "_s")) - get(var))^2))
-      }
-      for (j in seq_along(return_periods)) {
-        interval = quantile(est_return_levels[j, ], c(.025, .975))
-        is_inside = c(is_inside, return_levels[j] >= interval[1] && return_levels[j] <= interval[2])
-        lower = c(lower, interval[1]); upper = c(upper, interval[2])
-        mean_vals = c(mean_vals, base::mean(est_return_levels[j, ]))
-        mse = c(mse, base::mean((est_return_levels[j, ] - return_levels[j])^2))
-      }
-      stwcrps = base::mean(stwcrps_bgev(y, μ_s, σ_s, ξ_s, .9))
-      etwcrps = expected_twcrps_bgev_gev(μ_s, σ_s, ξ_s, .9, μ_true = μ, σ_true = σ, ξ_true = ξ)
-      S = abs(expected_twcrps_bgev(μ_s, σ_s, ξ_s, .9))
-      estwcrps = etwcrps / S + log(S)
-      #stwcrps_true = mean(stwcrps_bgev(y, μ, σ, ξ, .9))
-      stats[[n]] = data.frame(
-        par = pars,
-        val = values,
-        is_inside = is_inside,
-        n = n, i = i,
-        lower = lower,
-        mean = mean_vals,
-        mse = mse,
-        stwcrps = stwcrps,
-        etwcrps = etwcrps,
-        estwcrps = estwcrps,
-        #stwcrps_true = mean(stwcrps_true),
-        upper = upper,
-        CI_width = upper - lower,
-        stringsAsFactors = FALSE)
-    }
+    stats = lapply(
+      n_vec,
+      function(n) {
+        y = rgev(n, μ, σ, ξ)
+        res = tryCatch(inla_bgev(y, α = α, β = β), error = function(e) NULL)
+        if (is.null(res)) return(NULL)
+        samples = INLA::inla.posterior.sample(1000, res, seed = 1)
+        q_s = vapply(samples, function(x) utils::tail(x$latent, 1), 1) * res$standardising_const
+        s_s = vapply(samples, function(x) x$hyperpar[1], 1) * res$standardising_const
+        ξ_s = vapply(samples, function(x) x$hyperpar[2], 1)
+        locscale_s = locspread_to_locscale(q_s, s_s, ξ_s, α, β)
+        μ_s = locscale_s$μ; σ_s = locscale_s$σ
+        est_return_levels = mapply(return_level_bgev, μ = μ_s, σ = σ_s, ξ = ξ_s,
+                                   MoreArgs = list(period = return_periods))
+        pars = c("q", "s", "ξ", paste(return_periods, "return period"))
+        is_inside = NULL; lower = NULL; upper = NULL; mean_vals = NULL; mse = NULL
+        values = c(q, s, ξ, return_levels)
+        for (var in c("q", "s", "ξ")) {
+          interval = quantile(get(paste0(var, "_s")), c(.025, .975))
+          is_inside = c(is_inside, get(var) >= interval[1] && get(var) <= interval[2])
+          lower = c(lower, interval[1]); upper = c(upper, interval[2])
+          mean_vals = c(mean_vals, base::mean(get(paste0(var, "_s"))))
+          mse = c(mse, base::mean((get(paste0(var, "_s")) - get(var))^2))
+        }
+        for (j in seq_along(return_periods)) {
+          interval = quantile(est_return_levels[j, ], c(.025, .975))
+          is_inside = c(is_inside, return_levels[j] >= interval[1] && return_levels[j] <= interval[2])
+          lower = c(lower, interval[1]); upper = c(upper, interval[2])
+          mean_vals = c(mean_vals, base::mean(est_return_levels[j, ]))
+          mse = c(mse, base::mean((est_return_levels[j, ] - return_levels[j])^2))
+        }
+        stwcrps = base::mean(stwcrps_bgev(y, μ_s, σ_s, ξ_s, .9))
+        etwcrps = expected_twcrps_bgev_gev(μ_s, σ_s, ξ_s, .9, μ_true = μ, σ_true = σ, ξ_true = ξ)
+        S = abs(expected_twcrps_bgev(μ_s, σ_s, ξ_s, .9))
+        estwcrps = etwcrps / S + log(S)
+        #stwcrps_true = mean(stwcrps_bgev(y, μ, σ, ξ, .9))
+        data.frame(
+          par = pars,
+          val = values,
+          is_inside = is_inside,
+          n = n, i = i,
+          lower = lower,
+          mean = mean_vals,
+          mse = mse,
+          stwcrps = stwcrps,
+          etwcrps = etwcrps,
+          estwcrps = estwcrps,
+          #stwcrps_true = mean(stwcrps_true),
+          upper = upper,
+          CI_width = upper - lower,
+          stringsAsFactors = FALSE)
+      })
     stats = do.call(rbind, stats)
     message("Done with iter ", i, " out of ", n_trials)
     stats
@@ -99,6 +139,41 @@ saveRDS(
   stats,
   file.path(here::here(), "results", "simulation-study-univariate.rds"))
 
+stats %>%
+  do.call(rbind, .) %>%
+  dplyr::filter(n == tail(n_vec, 1), par == "s") %>%
+  dplyr::mutate(below = case_when(val < lower ~ TRUE, val > upper ~ FALSE, TRUE ~ NA)) %>%
+  ggplot() +
+  geom_point(aes(x = i, y = val, col = below)) +
+  geom_errorbar(aes(x = i, ymin = lower, ymax = upper, col = below)) +
+  facet_wrap(~is_inside)
+
+stats %>%
+  do.call(rbind, .) %>%
+  dplyr::filter(n == tail(n_vec, 1), par == "ξ") %>%
+  dplyr::mutate(below = case_when(val < lower ~ TRUE, val > upper ~ FALSE, TRUE ~ NA)) %>%
+  ggplot() +
+  geom_point(aes(x = i, y = val, col = below)) +
+  geom_errorbar(aes(x = i, ymin = lower, ymax = upper, col = below)) +
+  facet_wrap(~is_inside)
+
+stats %>%
+  do.call(rbind, .) %>%
+  dplyr::filter(n == tail(n_vec, 1), par == "s") %>%
+  dplyr::mutate(below = case_when(val < lower ~ TRUE, val > upper ~ FALSE, TRUE ~ NA)) %>%
+  ggplot() +
+  geom_point(aes(x = i, y = val, col = below)) +
+  geom_errorbar(aes(x = i, ymin = lower, ymax = upper, col = below)) +
+  facet_wrap(~is_inside)
+
+stats %>%
+  do.call(rbind, .) %>%
+  dplyr::filter(n == tail(n_vec, 1), par %in% c("s", "ξ"), !is_inside) %>%
+  dplyr::mutate(below = case_when(val < lower ~ TRUE, val > upper ~ FALSE, TRUE ~ NA)) %>%
+  ggplot() +
+  geom_point(aes(x = i, y = val, col = below)) +
+  geom_errorbar(aes(x = i, ymin = lower, ymax = upper, col = below)) +
+  facet_wrap(~par, scales = "free_y")
 
 stats %>%
   do.call(rbind, .) %>%
