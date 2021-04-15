2acfd7f062e6b4d4aeb0b769ab31a2152dd7ad53
Silius M. Vandeskog <siliusv@gmail.com>
2021-04-15 14:54:30 +0200

    Speed up simulation study

--- exec/simulation-study-out-of-sample.R
+++ exec/simulation-study-out-of-sample.R
@@ -38,19 +38,11 @@ n = 1500 # Number of samples
 n_loc = 250 # Number of "locations" that the data are sampled from
 n_leave_out_loc = 50 # Number of locations that are only used for testing
 α = .5; β = .8 # Probabilities used in the location and spread parameters
-n_trials = 200 # Number of times to perform the simulation study
+n_trials = 100 # Number of times to perform the simulation study
 block_size = 24 * 365 # The size of a block
 num_cores = 25 # Number of cores for parallelisation
-B = 100 # The number of bootstraps used in the two-step model for estimating s^*
+B = 50 # The number of bootstraps used in the two-step model for estimating s^*
 verbose = FALSE # Print a lot of progress messages?
-
-#n = 1500
-#n_loc = 50
-#n_leave_out_loc = 5
-#n_trials = 3
-#num_cores = 3
-#verbose = TRUE
-#B = 5
 
 # Set a seed that works when doing parallelisation
 set.seed(1, kind = "L'Ecuyer-CMRG")
@@ -67,8 +59,9 @@ res = parallel::mclapply(
       time = list())
 
     # Sample ξ and the regression coefficients for σ for every single observation
-    n_σ = sample.int(4, 1) + 4
-    σ_coeffs = c(runif(1, 1, 3), rnorm(n_σ, 0, .2)) / 10
+    #n_σ = sample.int(4, 1)
+    n_σ = 4
+    σ_coeffs = c(runif(1, .5, 2), rnorm(n_σ, 0, .2))
     ξ = rep(runif(1, .01, .4), n_loc)
     covariate_names = list(NULL, paste0("σ_", seq_len(n_σ)), NULL)
 
@@ -139,8 +132,8 @@ res = parallel::mclapply(
         covariate_names = covariate_names,
         s_est = rep(joint_res$standardising_const, n_loc),
         fun = list(
-	  μ = function(pars) locspread_to_locscale(pars$q, pars$s, pars$ξ, α, β)$μ,
-	  σ = function(pars) locspread_to_locscale(pars$q, pars$s, pars$ξ, α, β)$σ,
+          μ = function(pars) locspread_to_locscale(pars$q, pars$s, pars$ξ, α, β)$μ,
+          σ = function(pars) locspread_to_locscale(pars$q, pars$s, pars$ξ, α, β)$σ,
           r10 = get_return_level_function(10),
           r25 = get_return_level_function(25),
           r100 = get_return_level_function(100),
@@ -154,8 +147,8 @@ res = parallel::mclapply(
         s_list = list(rep(joint_res$standardising_const, n_loc)),
         verbose = verbose,
         fun = list(
-	  μ = function(pars) locspread_to_locscale(pars$q, pars$s, pars$ξ, α, β)$μ,
-	  σ = function(pars) locspread_to_locscale(pars$q, pars$s, pars$ξ, α, β)$σ,
+          μ = function(pars) locspread_to_locscale(pars$q, pars$s, pars$ξ, α, β)$μ,
+          σ = function(pars) locspread_to_locscale(pars$q, pars$s, pars$ξ, α, β)$σ,
           r10 = get_return_level_function(10),
           r25 = get_return_level_function(25),
           r100 = get_return_level_function(100),
@@ -296,12 +289,12 @@ res = parallel::mclapply(
       covariate_names = covariate_names,
       s_list = s_vals,
       fun = list(
-	  μ = function(pars) locspread_to_locscale(pars$q, pars$s, pars$ξ, α, β)$μ,
-	  σ = function(pars) locspread_to_locscale(pars$q, pars$s, pars$ξ, α, β)$σ,
+        μ = function(pars) locspread_to_locscale(pars$q, pars$s, pars$ξ, α, β)$μ,
+        σ = function(pars) locspread_to_locscale(pars$q, pars$s, pars$ξ, α, β)$σ,
         r10 = get_return_level_function(10),
         r25 = get_return_level_function(25),
         r100 = get_return_level_function(100),
-          r500 = get_return_level_function(500),
+        r500 = get_return_level_function(500),
         r50 = get_return_level_function(50)))
     # Use the samples to compute estimated parameters and return levels statistics at all locations
     twostep_stats = inla_stats(
@@ -311,12 +304,12 @@ res = parallel::mclapply(
       s_list = s_vals,
       verbose = verbose,
       fun = list(
-	  μ = function(pars) locspread_to_locscale(pars$q, pars$s, pars$ξ, α, β)$μ,
-	  σ = function(pars) locspread_to_locscale(pars$q, pars$s, pars$ξ, α, β)$σ,
+        μ = function(pars) locspread_to_locscale(pars$q, pars$s, pars$ξ, α, β)$μ,
+        σ = function(pars) locspread_to_locscale(pars$q, pars$s, pars$ξ, α, β)$σ,
         r10 = get_return_level_function(10),
         r25 = get_return_level_function(25),
         r100 = get_return_level_function(100),
-          r500 = get_return_level_function(500),
+        r500 = get_return_level_function(500),
         r50 = get_return_level_function(50)))
     # Compute scoring rules at all testing locations
     twostep_stwcrps = list()
@@ -374,116 +367,116 @@ res = parallel::mclapply(
       n_σ = n_σ,
       stringsAsFactors = FALSE)
 
-    # ==========================================================================
-    # Evaluate the performance of the two-step model, without bootstrapping
-    # ==========================================================================
+    # # ==========================================================================
+    # # Evaluate the performance of the two-step model, without bootstrapping
+    # # ==========================================================================
 
-    # Performe inference and report the time it takes
-    twostep_one_time = proc.time()
-    sd_res = do.call(inla, sd_inla_args)
-    sd_samples = exp(sd_res$summary.linear.predictor$mean)
-    twostep_one_res = tryCatch(
-      inla_bgev(
-        data = in_sample_df,
-        response_name = "y",
-        s_est = sd_samples[location_indices][location_indices > n_leave_out_loc],
-        diagonal = .1,
-        covariate_names =  covariate_names,
-        α = α,
-        β = β),
-      error = function(e) NULL)
-    twostep_one_time = proc.time() - twostep_one_time
-    twostep_one_time = sum(twostep_one_time[-3]) # That is just how it works...
+    # # Performe inference and report the time it takes
+    # twostep_one_time = proc.time()
+    # sd_res = do.call(inla, sd_inla_args)
+    # sd_samples = exp(sd_res$summary.linear.predictor$mean)
+    # twostep_one_res = tryCatch(
+    #   inla_bgev(
+    #     data = in_sample_df,
+    #     response_name = "y",
+    #     s_est = sd_samples[location_indices][location_indices > n_leave_out_loc],
+    #     diagonal = .1,
+    #     covariate_names =  covariate_names,
+    #     α = α,
+    #     β = β),
+    #   error = function(e) NULL)
+    # twostep_one_time = proc.time() - twostep_one_time
+    # twostep_one_time = sum(twostep_one_time[-3]) # That is just how it works...
 
-    # Estimate parameters, return levels and compute scoring rules, if INLA did not crash
-    if (!is.null(twostep_one_res)) {
-      # Sample from the estimated posterior
-      twostep_one_samples = INLA::inla.posterior.sample(500, twostep_one_res, seed = 1)
-      # Use the samples to compute estimated parameters and return levels at all locations
-      twostep_one_stats = inla_stats(
-        sample_list = list(twostep_one_samples),
-        data = dplyr::distinct(df, σ_1, .keep_all = TRUE),
-        covariate_names = covariate_names,
-        s_list = list(twostep_one_res$standardising_const * sd_samples),
-        verbose = verbose,
-        fun = list(
-	  μ = function(pars) locspread_to_locscale(pars$q, pars$s, pars$ξ, α, β)$μ,
-	  σ = function(pars) locspread_to_locscale(pars$q, pars$s, pars$ξ, α, β)$σ,
-          r10 = get_return_level_function(10),
-          r25 = get_return_level_function(25),
-          r100 = get_return_level_function(100),
-          r500 = get_return_level_function(500),
-          r50 = get_return_level_function(50)))
-      # Use the samples to compute estimated parameters and return levels statistics at all locations
-      twostep_one_pars = inla_bgev_pars(
-        samples = twostep_one_samples,
-        data = df,
-        covariate_names = covariate_names,
-        s_est = twostep_one_res$standardising_const * sd_samples,
-        fun = list(
-	  μ = function(pars) locspread_to_locscale(pars$q, pars$s, pars$ξ, α, β)$μ,
-	  σ = function(pars) locspread_to_locscale(pars$q, pars$s, pars$ξ, α, β)$σ,
-          r10 = get_return_level_function(10),
-          r25 = get_return_level_function(25),
-          r100 = get_return_level_function(100),
-          r500 = get_return_level_function(500),
-          r50 = get_return_level_function(50)))
-      # Compute scoring rules at all locations
-      twostep_one_stwcrps = list()
-      twostep_one_twcrps = list()
-      twostep_one_etwcrps = vector("numeric", n_leave_out_loc)
-      twostep_one_estwcrps = vector("numeric", n_leave_out_loc)
-      for (j in seq_len(n_leave_out_loc)) {
-        obs = y[base::which(location_indices == j)]
-        par = locspread_to_locscale(twostep_one_pars$q[j, ], twostep_one_pars$s[j, ],
-                                    twostep_one_pars$ξ[j, ], α, β)
-        if (verbose) message(j)
-        twostep_one_stwcrps[[j]] = stwcrps_bgev(obs, par$μ, par$σ, par$ξ, .9)
-        twostep_one_twcrps[[j]] = stwcrps_bgev(obs, par$μ, par$σ, par$ξ, .9)
-        etwcrps = expected_twcrps_bgev_gev(par$μ, par$σ, par$ξ, .9,
-                                       μ_true = truth$μ, σ_true = truth$σ, ξ_true = truth$ξ)
-        twostep_one_etwcrps[j] = etwcrps
-        S = expected_twcrps_bgev(par$μ, par$σ, par$ξ, .9)
-        twostep_one_estwcrps[j] = etwcrps / S + log(S)
-      }
-      twostep_one_stwcrps = unlist(twostep_one_stwcrps)
-      twostep_one_twcrps = unlist(twostep_one_twcrps)
-      # Test how good the interval estimates are
-      res$inclusion$twostep_one = lapply(
-        c("μ", "σ", "q", "s", "ξ", "r10", "r25", "r50", "r100", "r500"),
-        function(name) {
-          res = data.frame(
-            name = name,
-            value = truth[[name]],
-            lower = twostep_one_stats[[name]]$`2.5%`,
-            upper = twostep_one_stats[[name]]$`97.5%`,
-            mean = twostep_one_stats[[name]]$mean,
-            in_sample = seq_len(n_loc) > n_leave_out_loc,
-            n_σ = n_σ,
-            model = "twostep_one",
-            stringsAsFactors = FALSE)
-          res$err = res$value - res$mean
-          res$included = truth[[name]] > res$lower & truth[[name]] < res$upper
-          res$mse = base::mean((truth[[name]] - twostep_pars[[name]])^2)
-          res
-        }) %>%
-        do.call(rbind, .)
-      # Return the mean of all the computed scores
-      res$score$twostep_one = data.frame(
-        stwcrps = base::mean(twostep_one_stwcrps),
-        twcrps = base::mean(twostep_one_twcrps),
-        etwcrps = base::mean(twostep_one_etwcrps),
-        estwcrps = base::mean(twostep_one_estwcrps),
-        model = "twostep_one",
-        n_σ = n_σ,
-        stringsAsFactors = FALSE)
-      # Return the time it took to run the inference
-      res$time$twostep_one = data.frame(
-        time = twostep_one_time,
-        model = "twostep_one",
-        n_σ = n_σ,
-        stringsAsFactors = FALSE)
-    }
+    # # Estimate parameters, return levels and compute scoring rules, if INLA did not crash
+    # if (!is.null(twostep_one_res)) {
+    #   # Sample from the estimated posterior
+    #   twostep_one_samples = INLA::inla.posterior.sample(500, twostep_one_res, seed = 1)
+    #   # Use the samples to compute estimated parameters and return levels at all locations
+    #   twostep_one_stats = inla_stats(
+    #     sample_list = list(twostep_one_samples),
+    #     data = dplyr::distinct(df, σ_1, .keep_all = TRUE),
+    #     covariate_names = covariate_names,
+    #     s_list = list(twostep_one_res$standardising_const * sd_samples),
+    #     verbose = verbose,
+    #     fun = list(
+    #       μ = function(pars) locspread_to_locscale(pars$q, pars$s, pars$ξ, α, β)$μ,
+    #       σ = function(pars) locspread_to_locscale(pars$q, pars$s, pars$ξ, α, β)$σ,
+    #       r10 = get_return_level_function(10),
+    #       r25 = get_return_level_function(25),
+    #       r100 = get_return_level_function(100),
+    #       r500 = get_return_level_function(500),
+    #       r50 = get_return_level_function(50)))
+    #   # Use the samples to compute estimated parameters and return levels statistics at all locations
+    #   twostep_one_pars = inla_bgev_pars(
+    #     samples = twostep_one_samples,
+    #     data = df,
+    #     covariate_names = covariate_names,
+    #     s_est = twostep_one_res$standardising_const * sd_samples,
+    #     fun = list(
+    #       μ = function(pars) locspread_to_locscale(pars$q, pars$s, pars$ξ, α, β)$μ,
+    #       σ = function(pars) locspread_to_locscale(pars$q, pars$s, pars$ξ, α, β)$σ,
+    #       r10 = get_return_level_function(10),
+    #       r25 = get_return_level_function(25),
+    #       r100 = get_return_level_function(100),
+    #       r500 = get_return_level_function(500),
+    #       r50 = get_return_level_function(50)))
+    #   # Compute scoring rules at all locations
+    #   twostep_one_stwcrps = list()
+    #   twostep_one_twcrps = list()
+    #   twostep_one_etwcrps = vector("numeric", n_leave_out_loc)
+    #   twostep_one_estwcrps = vector("numeric", n_leave_out_loc)
+    #   for (j in seq_len(n_leave_out_loc)) {
+    #     obs = y[base::which(location_indices == j)]
+    #     par = locspread_to_locscale(twostep_one_pars$q[j, ], twostep_one_pars$s[j, ],
+    #                                 twostep_one_pars$ξ[j, ], α, β)
+    #     if (verbose) message(j)
+    #     twostep_one_stwcrps[[j]] = stwcrps_bgev(obs, par$μ, par$σ, par$ξ, .9)
+    #     twostep_one_twcrps[[j]] = stwcrps_bgev(obs, par$μ, par$σ, par$ξ, .9)
+    #     etwcrps = expected_twcrps_bgev_gev(par$μ, par$σ, par$ξ, .9,
+    #                                    μ_true = truth$μ, σ_true = truth$σ, ξ_true = truth$ξ)
+    #     twostep_one_etwcrps[j] = etwcrps
+    #     S = expected_twcrps_bgev(par$μ, par$σ, par$ξ, .9)
+    #     twostep_one_estwcrps[j] = etwcrps / S + log(S)
+    #   }
+    #   twostep_one_stwcrps = unlist(twostep_one_stwcrps)
+    #   twostep_one_twcrps = unlist(twostep_one_twcrps)
+    #   # Test how good the interval estimates are
+    #   res$inclusion$twostep_one = lapply(
+    #     c("μ", "σ", "q", "s", "ξ", "r10", "r25", "r50", "r100", "r500"),
+    #     function(name) {
+    #       res = data.frame(
+    #         name = name,
+    #         value = truth[[name]],
+    #         lower = twostep_one_stats[[name]]$`2.5%`,
+    #         upper = twostep_one_stats[[name]]$`97.5%`,
+    #         mean = twostep_one_stats[[name]]$mean,
+    #         in_sample = seq_len(n_loc) > n_leave_out_loc,
+    #         n_σ = n_σ,
+    #         model = "twostep_one",
+    #         stringsAsFactors = FALSE)
+    #       res$err = res$value - res$mean
+    #       res$included = truth[[name]] > res$lower & truth[[name]] < res$upper
+    #       res$mse = base::mean((truth[[name]] - twostep_pars[[name]])^2)
+    #       res
+    #     }) %>%
+    #     do.call(rbind, .)
+    #   # Return the mean of all the computed scores
+    #   res$score$twostep_one = data.frame(
+    #     stwcrps = base::mean(twostep_one_stwcrps),
+    #     twcrps = base::mean(twostep_one_twcrps),
+    #     etwcrps = base::mean(twostep_one_etwcrps),
+    #     estwcrps = base::mean(twostep_one_estwcrps),
+    #     model = "twostep_one",
+    #     n_σ = n_σ,
+    #     stringsAsFactors = FALSE)
+    #   # Return the time it took to run the inference
+    #   res$time$twostep_one = data.frame(
+    #     time = twostep_one_time,
+    #     model = "twostep_one",
+    #     n_σ = n_σ,
+    #     stringsAsFactors = FALSE)
+    # }
 
     message("Done with iter nr. ", i)
 
@@ -529,7 +522,7 @@ res$score %>%
     etwcrps = format(base::mean(etwcrps), digits = 6),
     estwcrps = format(base::mean(estwcrps), digits = 6))
 
-nn = "etwcrps"
+nn = "estwcrps"
 joint_score = dplyr::filter(res$score, model == "joint")[[nn]]
 twostep_score = dplyr::filter(res$score, model == "twostep")[[nn]]
 diff_score = joint_score - twostep_score
@@ -539,7 +532,7 @@ diff_bootstraps = vapply(
   function(i) base::mean(sample(diff_score, length(diff_score), replace = TRUE)),
   numeric(1))
 c(mean = mean(diff_bootstraps),
-  quantile(diff_bootstraps, c(0, .025, .05, .25, .5, .75, .95, .975, 1)))
+  quantile(diff_bootstraps, c(0, .025, .05, .075, .1, .15, .2, .25)))
 # Significant difference between etwcrps of joint model and twostep model on the 5% level
 
 nn = "etwcrps"
@@ -558,9 +551,6 @@ permutation_diffs = vapply(
   numeric(1))
 mean(abs(permutation_diffs) > abs(mean(diff_score)))
 
-
-
-
 nn = "etwcrps"
 joint_score = dplyr::filter(res$score, model == "joint")[[nn]]
 twostep_one_score = dplyr::filter(res$score, model == "twostep_one")[[nn]]
@@ -570,7 +560,7 @@ diff_bootstraps = vapply(
   function(i) base::mean(sample(diff_score, length(diff_score), replace = TRUE)),
   numeric(1))
 c(mean = mean(diff_bootstraps),
-  quantile(diff_bootstraps, c(0, .025, .05, .25, .5, .75, .95, .975, 1)))
+  quantile(diff_bootstraps, c(0, .025, .05, .075, .1, .15, .2, .25)))
 
 time_stats = res$time %>%
   dplyr::group_by(model) %>%
@@ -580,14 +570,14 @@ time_stats = res$time %>%
 base::print(time_stats)
 
 percentages = res$inclusion %>%
-  dplyr::group_by(name, model, in_sample) %>%
+  dplyr::group_by(name, model, n_σ) %>%
   dplyr::mutate(percentage = base::mean(included)) %>%
-  dplyr::select(name, model, percentage, in_sample) %>%
+  dplyr::select(name, model, percentage, n_σ) %>%
   dplyr::slice(1)
 
 ggplot(percentages) +
   geom_col(aes(x = name, y = percentage, fill = model), position = "dodge") +
-  facet_wrap(~in_sample) +
+  facet_wrap(~n_σ) +
   geom_hline(yintercept = .95) +
   coord_cartesian(y = c(.5, 1))
 
